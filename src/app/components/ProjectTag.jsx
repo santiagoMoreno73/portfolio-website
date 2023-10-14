aad8a4ad87bbff00491f2d2d365cdabb22e95bb8
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-14 15:09:02 -0500

    added projectTag

--- src/app/components/ProjectTag.jsx
+++ src/app/components/ProjectTag.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const ProjectTag = () => {
+const ProjectTag = ({ tag, onClick, isSelected }) => {
+  const buttonStyle = isSelected 
+    ? "text-white border-purple-500" 
+    : "text-[#ADB7BE] border-slate-600 hover:border-white";
+  
   return (
-    <div>ProjectTag</div>
+    <button 
+      className={`${buttonStyle} rounded-full border-2 px-6 py-3 text-xl cursor-pointer`}
+      onClick={() => onClick(tag)}
+    >
+      {tag}
+    </button>
   )
 }
 
