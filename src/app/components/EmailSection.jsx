3c22d9690c2424fa8a550abcfc15cc0cb2e4b338
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-16 17:54:04 -0500

    added email section

---
+++ src/app/components/EmailSection.jsx
@@ -0,0 +1,120 @@
+"use client";
+import Link from 'next/link';
+import React, { useState } from 'react';
+import GithubIcon from "../../../public/images/github-icon.svg";
+import LinkedinIcon from "../../../public/images/linkedin-icon.svg";
+import Image from 'next/image';
+
+const EmailSection = () => {
+    const [emailSubmitted, setEmailSubmitted] = useState(false);
+
+    const handleSubmit = async(event) => {
+        event.preventDefault();
+        const data = {
+            email: event.target.email.value,
+            subject: event.target.subject.value,
+            message: event.target.message.value,
+        };
+
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "api/send";
+
+        // form the request for sending data to the server. 
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSONdata,
+        };
+
+        const response = await fetch(endpoint, options);
+        const resData = await response.json();
+        console.log("resData", resData);
+        
+        if (response.status === 200) {
+            setEmailSubmitted(true);
+            console.log("Message sent.");
+        }
+    }
+
+
+  return (
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
+        <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'></div>
+        <div className='z-10'>
+            <h5 className='text-xl font-bold text-white my-2'>
+                Let´s Connet
+            </h5>
+            <p className="text-[#ADB7BE] mb-4 max-w-md">
+                {" "}
+                I´m currently looking for new opportunities, my inbox is always open.
+                Whether you have a question or just want to say hi, I´ll try my best
+                to get back to you!
+            </p>
+            <div className='socials flex flex-row gap-2'>
+                <Link href="github.com">
+                    <Image src={GithubIcon} alt="Github Icon"/>
+                </Link>
+                <Link href="linkedin.com">
+                    <Image src={LinkedinIcon} alt="Linkedin Icon"/>
+                </Link>
+            </div>
+        </div>
+        <div>
+            <form className='flex flex-col gap-6' onSubmit={handleSubmit}>
+                <div className='mb-4'>
+                    <label htmlFor="email" type="email" className='text-white block mb-2 text-sm font-medium'>
+                        Your email
+                    </label>
+                    <input 
+                        name="email"
+                        type="email" 
+                        id="email" 
+                        required 
+                        className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                        placeholder="example@gmail.com"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label htmlFor="subject" className='text-white block mb-2 text-sm font-medium'>
+                        Subject
+                    </label>
+                    <input 
+                        name="subject"
+                        type="text" 
+                        id="subject" 
+                        required 
+                        className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                        placeholder="Just saying hi"
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label htmlFor="message" className='text-white block mb-2 text-sm font-medium'>
+                        Message
+                    </label>
+                    <texarea 
+                        name="message"
+                        id="message"
+                        className='bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5'
+                        placeholder="Let´s talk about ..."
+                    />
+                </div>
+                <button className='bg-purple-500 hover:bg-purple-600 text-white font-medium py-2.5 px-5 rounded-lg w-full'>
+                    Send Message
+                </button>    
+                {
+                    // If the email was submitted succesfully, show a success message.
+                    emailSubmitted && (
+                        <p className='text-green-500 text-sm mt-2'>
+                            Email sent succesfully!
+                        </p>
+                    )
+                }
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default EmailSection
