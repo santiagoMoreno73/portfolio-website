7d9d3f95c5e6d02f67f19631e9ea3bb6556d7ac8
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-16 18:10:54 -0500

    bug fixed in text animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -7,13 +7,8 @@ const HeroSection = () => {
   return (
     <section className="lg:py-16">
         <div className="grid grid-cols-1 sm:grid-cols-12">
-            <div className='col-span-7 place-self-center text-center sm:text-left'>
-                <h1 className='
-                    text-white mb-4 
-                    text-4xl lg:text-6xl
-                    sm:text-5xl 
-                    lg:leading-normal
-                    font-extrabold'>
+            <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
+                <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold'>
                     <span className='
                         text-transparent 
                         bg-clip-text bg-gradient-to-r 
@@ -68,7 +63,7 @@ const HeroSection = () => {
                 </div>
             </div>
 
-            <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+            <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
                 <div className='
                     rounded-full bg-[#181818] 
                     w-[250px] h-[250px]
