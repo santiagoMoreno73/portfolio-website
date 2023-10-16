19c532fc18ecc6fe744c355734ed9fa294b82fe9
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-16 18:10:39 -0500

    bug fixed in logo footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
     <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent border-b-transparent text-white'>
         <div className='container p-12 flex justify-between'>
-          <span>
+          <span className='z-10'>
               LOGO
           </span>
           <p className='text-slate-600'>
