ea74d8830d82a587182a7e43eaa720a8e2ca4d18
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-17 00:21:43 -0500

    added animated numbers

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -1,37 +1,58 @@
+"use client";
 import React from 'react';
+import dynamic from "next/dynamic";
+const AnimatedNumbers = dynamic(() => import("react-animated-numbers"), {
+  ssr: false,
+});
 
-const achivementsList = [
+const achievementsList = [
     {
-        metric: "Projects",
-        value: "100+" 
+      metric: "Projects",
+      value: "100",
+      postfix: "+",
     },
     {
-        metric: "Users",
-        value: "1M+" 
+      prefix: "~",
+      metric: "Users",
+      value: "100,000",
     },
     {
-        metric: "Awards",
-        value: "10+" 
+      metric: "Awards",
+      value: "7",
     },
     {
-        metric: "Years",
-        value: "2+" 
+      metric: "Years",
+      value: "5",
     },
-]
+  ];
 
 const AchievementsSection = () => {
   return (
     <div className='py-8 px-4 xl:gap-16 sm:py-16 xl:px-16'>
-        <div className='border-[#33354F] border rounded-md py-8 px-17 flex flex-row items-center justify-between'>
+        <div className='border-[#33354F] border rounded-md py-8 px-16 flex flex-row items-center justify-between'>
             {
-                achivementsList.map((archivement, index) => {
+                achievementsList.map((achievement, index) => {
                     return (
                         <div key={ index } className='flex flex-col items-center justify-center mx-4'>
-                            <h2 className='text-white text-4xl font-bold'>
-                                { archivement.value }
+                            <h2 className='text-white text-4xl font-bold flex flex-row'>
+                                { achievement.prefix }
+                                <AnimatedNumbers
+                                    includeComma
+                                    animateToNumber={parseInt(achievement.value)}
+                                    locale="en-US"
+                                    className="text-white text-4xl font-bold"
+                                    config={(_, index) => {
+                                        return {
+                                            mass: 1,
+                                            friction: 100,
+                                            tensions: 140 * (index + 1),
+                                        };
+                                    }}
+                                />
+                                { achievement.postfix }
                             </h2>
                             <p className='text-[#ADB7BE] text-base'>
-                                { archivement.metric }
+                                { achievement.metric }
                             </p>
                         </div>
                     );
