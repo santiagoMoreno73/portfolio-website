18ecb3f00b8553a0b0d237d250c391fdff7d926b
santiagoMoreno73 <santimor2009@hotmail.com>
2023-10-18 21:43:10 -0500

    added animation tabbutton

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { motion } from "framer-motion";
+
+const variants = {
+  default: { width: 0 }, 
+  active: { width: "calc(100% - 0.75rem)" },
+};
 
 const TabButton = ({ active, selectTab, children }) => {
-
-    // console.log(active, selectTab, children);
-    const buttonClass = active ? "text-white border-b border-primary" : "text-[#ADB7BE] border-b border-primary-500";
+  const buttonClass = active ? "text-white" : "text-[#ADB7BE]";
 
   return (
     <span onClick={selectTab}>
         <p className={`cursor-pointer mr-3 font-semibold hover:text-white ${buttonClass}`}>
             {children}
         </p>
+        <motion.div
+          animate={active ? "active" : "default"}
+          variants={variants}
+          className='h-1 bg-primary-500 mt-1 mr-3'
+        >
+        </motion.div>
     </span>
   )
 }
