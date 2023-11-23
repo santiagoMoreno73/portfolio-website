e4e020bec9a948718cf96f942562103cc383dea8
santiagoMoreno73 <santimor2009@hotmail.com>
2023-11-22 20:40:03 -0500

    fix navbar

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,9 +1,10 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import React, { useState } from "react";
-import NavLink from "./NavLink";
 // icons
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// components
+import NavLink from "./NavLink";
 import MenuOverlay from "./MenuOverlay";
 
 const navLinks = [
@@ -12,23 +13,26 @@ const navLinks = [
   { title: "Contact", path: "#contact" },
 ];
 
-const Navbar = () => {
+const Navbar = ({ handleChangeTheme, icon }) => {
   const [navOpen, setNavOpen] = useState(false);
 
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth > 960 && setNavOpen(false)
+    );
+  }, []);
+
   return (
-    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100">
-      <div
-        className="
-            flex container lg:py-4
-            flex-wrap items-center justify-between 
-            mx-auto px-4 py-2"
-      >
+    <nav className="fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-20 bg-[#202023] bg-opacity-100">
+      <div className="flex container lg:py-flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
         >
           LOGO
         </Link>
+
         <div className="mobile-menu block md:hidden">
           {!navOpen ? (
             <button
--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+// components
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
@@ -9,7 +9,7 @@ import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
+    <main className="flex min-h-screen flex-col bg-[#202023]">
       <Navbar />
       <container className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
